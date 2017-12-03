0ce0bb886c5f07da57b9f9f92bb3819537605e6f
Rob Moran <rob.moran@arm.com>
2017-12-03 23:51:01 +0000

    Use gh-pages for build branch

--- types/server.d.ts
+++
@@ -1,6 +0,0 @@
-import { BluetoothDevice } from "./device";
-export declare class BluetoothRemoteGATTServer {
-    _services: Array<string>;
-    device: BluetoothDevice;
-    connected: boolean;
-}
