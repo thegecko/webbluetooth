f1b4f4f609a4a5e304a727c7ba18b69a8e62bb6f
thegecko <github@thegecko.org>
2023-03-22 23:43:58 +0000

    More tests

--- test/bluetooth.test.js
+++ test/bluetooth.test.js
@@ -1,8 +1,61 @@
 const assert = require('assert');
-const bluetooth = require('../').bluetooth
+const bluetooth = require('../').bluetooth;
 
 describe('bluetooth module', () => {
     it('should describe basic constants', () => {
         assert.notEqual(bluetooth, undefined, 'bluetooth must not be undefined');
+    });
+});
+
+describe('requestDevice', () => {
+    it('should return a device', async () => {
+        const device = await bluetooth.requestDevice({
+            filters: [{ namePrefix: 'BBC micro:bit' }]
+        });
+        assert.notEqual(device, undefined);
+        // await new Promise(resolve => setTimeout(resolve, 5000));
+    });
+});
+
+/*
+describe('getDevices', () => {
+    it('should return one device', async () => {
+        const device = await bluetooth.requestDevice({
+            filters: [{ namePrefix: 'BBC micro:bit' }]
+        });
+        const devices = await bluetooth.getDevices();
+        assert.equal(devices.length, 1);
+        assert.notEqual(devices[0], undefined);
+        assert.deepEqual(devices[0], device);
+    });
+});
+
+*/
+describe('device', () => {
+    let device;
+
+    before(async () => {
+        device = await bluetooth.requestDevice({
+            filters: [{ namePrefix: 'BBC micro:bit' }]
+        });
+    });
+
+    it('should have device properties', () => {
+        assert.notEqual(device.id, undefined);
+        assert.notEqual(device.gatt, undefined);
+        assert.equal(device.name.startsWith('BBC micro:bit ['), true);
+    });
+
+    it('should have gatt properties', () => {
+        assert.equal(device.gatt.connected, false);
+        assert.deepEqual(device.gatt.device, device);
+    });
+
+    it('should connect and disconnect', async () => {
+        assert.equal(device.gatt.connected, false);
+        await device.gatt.connect();
+        assert.equal(device.gatt.connected, true);
+        await device.gatt.disconnect();
+        assert.equal(device.gatt.connected, false);
     });
 });
